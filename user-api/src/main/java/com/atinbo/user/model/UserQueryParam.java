ca5f659439a7b7b9d55bdacf8ea5dc4ba581ee4d
曾浩 <epdcgsi@dingtalk.com>
2019-07-20 11:51:21 +0800

    hibernate条件查询自动封装

--- user-api/src/main/java/com/atinbo/user/model/UserQueryParam.java
+++ user-api/src/main/java/com/atinbo/user/model/UserQueryParam.java
@@ -2,6 +2,9 @@ package com.atinbo.user.model;
 
 
 import com.atinbo.core.service.model.PageParam;
+import com.atinbo.user.search.Operator;
+import com.atinbo.user.search.Query;
+import com.atinbo.user.search.QueryParam;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -11,10 +14,14 @@ import lombok.experimental.Accessors;
  */
 @Data
 @Accessors(chain = true)
-public class UserQueryParam extends PageParam {
+public class UserQueryParam extends PageParam implements QueryParam {
     private Long userId;
+    @Query(operator = Operator.LIKE,field = "nickName")
     private String nickname;
     private Integer age;
+
+    @Query(ignore = true)
     private String direction;
+    @Query(ignore = true)
     private String property;
 }
---
+++ user-api/src/main/java/com/atinbo/user/search/Operator.java
@@ -0,0 +1,10 @@
+package com.atinbo.user.search;
+
+/**
+ * 查询操作符
+ * @author zenghao
+ * @date 2019-07-20
+ */
+public enum Operator{
+    EQ, LIKE, GT, LT, GTE, LTE, NOTNULL, ISNULL, ENUMEQ;
+}
